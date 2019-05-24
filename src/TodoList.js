f5e1b8cac8b57b4ce0a3eac51a013860b45da73b
pferlito <pferlito@gmail.com>
2019-05-24 05:32:22 -0700

    Move destroy to TodoList.

--- src/TodoList.js
+++ src/TodoList.js
@@ -7,12 +7,17 @@ class TodoList extends Component {
   }
   handleChange = (index) => {
     const states = ['active','completed'];
-    const currentState = this.props.items[index].state;
+    const currentState = this.props.list.items[index].state;
     const currentStateIndex = states.indexOf(currentState);
     const newState = currentStateIndex ? 0 : 1;
-    const mutatedState = [...this.props.items];
+    const mutatedState = [...this.props.list.items];
     mutatedState[index].state = states[newState];
     this.setState(mutatedState);
+  };
+  handleDestroy = (index) => {
+    const mutatedItems = {...this.props.list};
+    mutatedItems.items.splice(index,1);
+    this.setState(mutatedItems);
   };
   render() {
     let self = this;
@@ -21,14 +26,14 @@ class TodoList extends Component {
         <input id="toggle-all" className="toggle-all" type="checkbox"/>
         <label htmlFor="toggle-all">Mark all as complete</label>
         <ul className="todo-list">
-          {this.props.items.map(
+          {this.props.list.items.map(
             function (value, index) {
               return (
                 <li key={index} className={value.state === 'completed' ? 'completed' : ''}>
                   <div className="view">
                     <Toggle value = {value}
                       handleChange = {self.handleChange}
-                      handleDestroy = {self.props.handleDestroy}
+                      handleDestroy = {self.handleDestroy}
                       index={index}/>
                   </div>
                 </li>
