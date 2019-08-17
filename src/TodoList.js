4bb3b1f22d6a236bfdb4039458c845d14a43fd4e
pferlito <pferlito@gmail.com>
2019-08-17 03:54:54 -0700

    Convert TodoList.

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class TodoList extends Component {
-  render() {
-    return (
-      <section className="main">
-        <input id="toggle-all" className="toggle-all" type="checkbox" />
-          <label htmlFor="toggle-all">Mark all as complete</label>
-          <ul className="todo-list"></ul>
-      </section>
-    )
-  }
+function TodoList(props) {
+  return (
+    <section className="main">
+      <input id="toggle-all" className="toggle-all" type="checkbox" />
+      <label htmlFor="toggle-all">Mark all as complete</label>
+      <ul className="todo-list"></ul>
+    </section>
+  )
 }
 
 export default TodoList;
