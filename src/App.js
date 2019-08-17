e59ec5894f257e2b8880f585a046692ad947482a
pferlito <pferlito@gmail.com>
2019-08-17 04:18:36 -0700

    Use useState() hook to manage state.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import TodoList from './TodoList.js';
 import Header from './Header.js';
 import Footer from './Footer.js';
 
 function App() {
+  let state = {
+    items: ['one','two'],
+    filter: ""
+  };
+  let current_state;
+  let setState;
+  [current_state,setState] = useState(state);
+
   return (
     <section className="todoapp">
       <Header />
-      <TodoList/>
+      <TodoList list={current_state}/>
       <Footer />
     </section>
   );
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
 function TodoList(props) {
+  const filteredItems = props.list.items;
+
   return (
     <section className="main">
-      <input id="toggle-all" className="toggle-all" type="checkbox" />
+      <input id="toggle-all" className="toggle-all" type="checkbox"/>
       <label htmlFor="toggle-all">Mark all as complete</label>
-      <ul className="todo-list"></ul>
+      <ul className="todo-list">
+        {filteredItems.map(
+          function (value, index) {
+            return (
+              <li key={index}
+                  className={value.state === 'completed' ? 'completed' : ''}>
+                <div className="view">
+                  {value}
+                </div>
+              </li>
+            )
+          }
+        )}
+      </ul>
     </section>
   )
 }
