ed7fcb6f2e778dcb1282d3250a3cbab3f7d01457
pferlito <pferlito@gmail.com>
2019-08-17 16:46:24 -0700

    Add header component.

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
 function Header(props) {
+  let textInput = null;
+
+  const handleAdd = (e) => {
+    console.log(e);
+    console.log(textInput);
+    e.preventDefault();
+    const value = textInput.value;
+    props.handleAdd(value);
+    textInput.value = '';
+  };
+
   return (
     <header className="header">
       <h1>todos</h1>
-      <input className="new-todo" placeholder="What needs to be done?" autoFocus="" />
+      <form onSubmit={handleAdd}>
+        <input className="new-todo" ref={(input) => {
+          textInput = input
+        }}
+               placeholder="What needs to be done?"
+               autoFocus=""/>
+      </form>
     </header>
   )
 }
