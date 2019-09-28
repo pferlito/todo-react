ba9fe05b5d9294283cf900b7cffc480cc8684084
pferlito <pferlito@gmail.com>
2019-09-28 03:32:15 -0700

    Code factoring.

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,6 +4,13 @@ import Toggle from './Toggle.js';
 import ToggleAll from './ToggleAll.js';
 
 class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.handleDoubleClick = this.handleDoubleClick.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+  }
   handleDoubleClick(index) {
     this.props.handleStateChange(index, 'editing');
   }
@@ -26,7 +33,7 @@ class TodoList extends Component {
     let filteredItems = this.props.list.items;
     // filter items according to selected filter
     if (this.props.list.filter) {
-      filteredItems = this.props.list.items.filter((el, idx) => {
+      filteredItems = this.props.list.items.filter((el) => {
         return el.state === this.props.list.filter;
       });
     }
@@ -37,25 +44,24 @@ class TodoList extends Component {
           handleToggleAll={() => self.props.handleToggleAll()}
         />
         <ul className="todo-list">
-          {filteredItems.map(
-            function (value, index) {
+          {filteredItems.map((value, index) => {
               return (
                 <li key={index}
-                    onDoubleClick={(e) => self.handleDoubleClick(index)}
+                    onDoubleClick={() => this.handleDoubleClick(index)}
                     className={value.state}>
                   <div className="view">
                     <Toggle value={value}
                       handleStateChange={(index, newState) => self.props.handleStateChange(index, newState)}
-                      handleToggleClick={(index) => self.handleToggleClick(index)}
-                      handleDestroy={(index) => self.props.handleDestroy(index)}
+                      handleToggleClick={(index) => this.handleToggleClick(index)}
+                      handleDestroy={(index) => this.props.handleDestroy(index)}
                       index={index}/>
                   </div>
                   {value.state === 'editing' &&
                   <input
                     type="text"
                     className="edit"
-                    onChange={(e) => self.handleTextChange(e,index)}
-                    onBlur={(e) => {self.handleBlur(index)}}
+                    onChange={(e) => this.handleTextChange(e,index)}
+                    onBlur={() => {this.handleBlur(index)}}
                     defaultValue={value.text}
                   />}
                 </li>
